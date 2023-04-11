991cca609a660328768387ca3d59b0e56d9a7e2e
30621 <129130042+yongcarat@users.noreply.github.com>
2023-04-11 01:14:25 +0000

    if에 따른 참, 거짓 1. 5>3 참이므로 A,B 2. 5+3=8 참이므로 C.D 3. 5-5=0 거짓이므로 X 4. 5>3 참이므로 F,G 5. 5<3 거짓으므로 X

--- .ccls-cache/@home@runner@Woolala/or.c
+++ .ccls-cache/@home@runner@Woolala/or.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
- main()
-{
-     int a=15;
-     int b=20;
-     int c =a|b;
+// #include <stdio.h>
+//  main()
+// {
+//      int a=15;
+//      int b=20;
+//      int c =a|b;
 
-     printf("OR 연산 결과 : %d" , c);
- }
+//      printf("OR 연산 결과 : %d" , c);
+//  }
