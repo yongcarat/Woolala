9c1d20e059f3e85222dbed0eaeffb699f4d90cf0
yongcarat <129130042+yongcarat@users.noreply.github.com>
2023-04-18 01:20:19 +0000

    else if 배우기!!!!!

---
+++ .ccls-cache/@home@runner@Woolala/else_if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main(){
+  int x;
+  printf("1번 방탄, 2번 세븐틴, 3번 스키즈, 4번 엑소, 5번 몬스타엑스\n");
+  scanf("%d", &x);
+  
+  if(x==1)
+    printf("방탄를 좋아하는군요!");
+  else if(x==2)
+    printf("세븐틴을 좋아하는군요!");
+  else if(x==3)
+    printf("스키즈를 좋아하는군요!");
+  else if(x==4)
+    printf("엑소를 좋아하는군요!");
+  else if(x==5)
+   printf("몬스타엑스를 좋아하는군요!");
+  else
+    printf("당신은 아마도 여자 그룹을 좋아하시는 것 같군요");
+  printf("좋아하는 보이그룹 조사 끝");
+}
