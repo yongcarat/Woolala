4a7a6f1363adc2d25e5b881d088dfa3350fc1646
yongcarat <129130042+yongcarat@users.noreply.github.com>
2023-05-09 01:16:45 +0000

    미션 3, 정수 2개 입력 후 큰 숫자 출력

---
+++ mission3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main (){
+  int a, b;
+  printf("정수를 입력하시오: ");
+   scanf("%d", &a);
+   scanf("%d", &b);
+  if (a>b) 
+    printf("%d", a);
+   else if (a<b)
+    printf("%d", b);
+  
+  
+}
